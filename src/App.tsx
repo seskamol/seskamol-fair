1f8740646e6c597f3b2afe37c9e80067e9606e3d
seskamol <semolacin@gmail.com>
2024-06-07 21:53:06 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ import {
 import { ColorModeContext, ColorMode } from './theme/ColorModeContext'
 //
 import CompConnect from './components/CompConnect'
-//import CompInfoCard from './components/CompInfoCard'
-//import CompThree from './components/CompThree'
+import CompInfoCard from './components/CompInfoCard'
+import CompThree from './components/CompThree'
 import CompLinks from './components/CompLinks'
 
 /////////////////////////////
@@ -77,8 +77,8 @@ function App() {
               <Button onClick={theme.toggleColorMode} size="small" sx={{ border: 1, borderColor: "secondary.light", width: '100%' }}> {theme.mode == 'dark' ? 'border' : 'close'} </Button>
 
               <CompConnect />
-              {/* <CompThree />
-              <CompInfoCard /> */}
+              <CompThree />
+              <CompInfoCard />
 
             </Box>
             <CompLinks />
