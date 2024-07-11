df8e4dfcf7b56095112946b52ec626d73563b43b
seskamol <semolacin@gmail.com>
2024-07-11 14:06:37 +0300

    commit

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -60,7 +60,7 @@ export const tokens = (mode: PaletteMode) => ({
 
 export const themeSettings = (mode: PaletteMode) => {
     //const colors = tokens(mode);
-    const letterSpacingM = "0.7033vw"; // letterSpacing
+    const letterSpacingM = 5; // letterSpacing
     const fontFamilyM = "Arial"; //fontFamily
 
     return {
@@ -140,7 +140,7 @@ export const themeSettings = (mode: PaletteMode) => {
 
         typography: {
             htmlFontSize: 15,
-            fontSize: 7,
+            fontSize: 4,
 
             fontFamily: fontFamilyM,
 
