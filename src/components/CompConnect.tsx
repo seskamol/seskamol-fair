46e846bebc8bb526af32ab8e7c97cec684458888
seskamol <semolacin@gmail.com>
2024-11-07 16:22:31 +0300

    commit
    main

--- src/components/CompConnect.tsx
+++ src/components/CompConnect.tsx
@@ -84,8 +84,8 @@ function CompConnect() {
                             variant="outlined"
                             //fullWidth="true"
                             sx={{ minWidth: '100px', pl: 1.6, mr: "0.4vw", pt: 0.08, pb: 0.03 }}
-                            //align={'center'}
-                            noWrap={true}
+                        //align={'center'}
+                        //noWrap={true}
                         >
                             {connector.name}
                         </Button>
